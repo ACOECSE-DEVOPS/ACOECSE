70fd959155b89f24d81649cda7e78c26b47ca8a7
ACOECSE-DEVOPS <113933870+ACOECSE-DEVOPS@users.noreply.github.com>
2022-10-13 11:54:13 +0530

    update city

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 	public static void main(String args[])
 	{
 		System.out.print("DURGA PRASAD welcome");
-		System.out.print("DURGA PRASAD welcome");
+		System.out.print("DURGA PRASAD RAJAHMUNDRY");
 		
 	}
 
